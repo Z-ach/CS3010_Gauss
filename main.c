6546c94762074bdba0bee06ccd54e4eb78bd0a92
z-ach <zachkaufman97@gmail.com>
2019-03-17 15:02:31 -0700

    works

--- main.c
+++ main.c
@@ -54,17 +54,23 @@ float **parse_file(char *filename, int *size){
 	for(int i = 0; i < mat_size + 1; i++){
 		int numb_buff_index = 0;
 		int numb_index = 0;
-		while((character = fgetc(file_pointer)) != '\n'){
-			if(character == ' '){
-				printf("buffer was %s\n", number_buffer);
+		int last_char_space = 0;
+		character = fgetc(file_pointer);
+		do{
+			//printf("running... buff currently %s, %d\n", number_buffer, last_char_space);
+			if(last_char_space == 1 && character == ' ') continue;
+			if(character == ' ' && number_buffer[0] != '\0'){
+				//printf("buffer was %s\n", number_buffer);
 				coeff[i][numb_index++] = atof(number_buffer);
 				memset(number_buffer, '\0', 50);
 				numb_buff_index = 0;
+				last_char_space = 1;
 			}
 			else{
 				number_buffer[numb_buff_index++] = character;
+				last_char_space = 0;
 			}
-		}
+		}while((character = fgetc(file_pointer)) != '\n');
 	}
 	fclose(file_pointer);
 	*size = mat_size;
@@ -75,9 +81,9 @@ void print_matrix(float **coeff, float *consts, int size){
 	for(int i = 0; i < size; i++){
 		printf("|");
 		for(int j = 0; j < size; j++){
-			printf("%4.1f ", coeff[i][j]);
+			printf("%15.5f ", coeff[i][j]);
 		}
-		printf("|\t|%4.1f|\n", consts[i]);
+		printf("|\t|%15.5f|\n", consts[i]);
 	}
 	printf("\n");
 }
@@ -99,20 +105,23 @@ void naive_gauss_elim(float **coeff, float *constants, int size){
 
 float *back_sub(float **coeff, float *constants, int size, int *order){
 	float *solution = malloc(size * sizeof(float));
-	solution[order[size - 1]] = constants[order[size - 1]] / coeff[size - 1][size - 1];
-	//printf("sol at %d was %f \n", order[size-1], solution[order[size-1]]);
+	solution[size - 1] = constants[order[size - 1]] / coeff[order[size - 1]][size - 1];
+	printf("sol = %f\n", solution[size-1]);
 	for(int i = size - 2; i >= 0; i--){
 		float sum = 0;
 		int curr_eq = order[i];
-		//printf("finding solution at %d\n", curr_eq);
+		printf("working on eq %d, sol will go into %d\n", curr_eq, i);
 		for(int j = i + 1; j < size; j++){
-			sum += solution[order[j]] * coeff[curr_eq][j];
-			//printf("added %f * %f = %f\n", solution[order[j]], coeff[curr_eq][j], solution[order[j]] * coeff[curr_eq][j]);
+			sum += solution[j] * coeff[curr_eq][j];
 		}
-		//printf("eval %f - %f = %f\n", constants[curr_eq], sum, constants[curr_eq] - sum);
-		//printf("div by %f\n", coeff[curr_eq][i]);
-		solution[order[i]] = (constants[curr_eq] - sum) / coeff[curr_eq][i];
+		solution[i] = (constants[curr_eq] - sum) / coeff[curr_eq][i];
+		printf("(%f - %f) / %f = %f\n", constants[curr_eq], sum, coeff[curr_eq][i], solution[i]); 
+		printf("sol to %d is %f\n", i, solution[i]);
 	}
+	for(int i = 0; i < size; i++){
+		//printf("%d ", order[i]);
+		printf("%f ", solution[i]);
+	}printf("\n");
 	return solution;
 }
 
@@ -151,8 +160,24 @@ int *partial_pivot_gauss(float **coeff, float *constants, int size){
             if(used[j] == 1) pivot_picker[j] = -1;
         }
         int piv_index = max(pivot_picker, size);
-        used[piv_index] = 1;
-        if(piv_index > i) swap(&order[piv_index], &order[i]);
+        printf("max was selected as %d, or %d\n", piv_index, order[piv_index]);
+        //if(!used[piv_index]) swap(&order[piv_index], &order[i]);
+        //if(!used[piv_index]) printf("swapped %d with %d\n", order[i], order[piv_index]);
+        int spot = 0;
+        for(int ind = 0; ind < size; ind++){
+			if(order[ind] == piv_index){
+				spot = ind;
+				break;
+			}
+        }
+        printf("spot was %d\n", spot);
+		swap(&order[spot], &order[i]);
+		printf("swapped %d with %d\n", order[i], order[spot]);
+		for(int i = 0; i < size; i++){
+			printf("%d ", order[i]);
+		}printf("\n");
+
+		used[piv_index] = 1;
 
         for(int j = 0; j < size; j++){
             if(used[j] == 1) continue;
